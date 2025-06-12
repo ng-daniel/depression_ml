more training scripts + data sample preprocessing analysis (fixed some data leaks too)

data.py
@@ -1,12 +1,15 @@
 import os
 import shutil
+import random
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from imblearn.over_sampling import SMOTE
 from scipy.fft import rfft
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split, KFold
+from scipy.ndimage import gaussian_filter1d
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.model_selection import StratifiedKFold
 import torch
 from torch.utils.data import Dataset, DataLoader
 
@@ -111,10 +114,10 @@ def load_dataframe_labels(dir_names: list, class_names: list, time: str = None):
 
 def kfolds_dataframes(data: pd.DataFrame, labels: list, numfolds: int, shuffle: bool, random_state: int = None):
     if shuffle:
-        kf = KFold(n_splits=numfolds, shuffle=shuffle, random_state=random_state)
+        kf = StratifiedKFold(n_splits=numfolds, shuffle=shuffle, random_state=random_state)
     else:
-        kf = KFold(n_splits=numfolds, shuffle=shuffle)
-    kf.get_n_splits(data)
+        kf = StratifiedKFold(n_splits=numfolds, shuffle=shuffle)
+    kf.get_n_splits(data, labels)
 
     folds = []
     for i, (train_index, test_index) in enumerate(kf.split(data)):
@@ -128,18 +131,65 @@ def kfolds_dataframes(data: pd.DataFrame, labels: list, numfolds: int, shuffle: 
     
     return folds
 
-def preprocess_train_test_dataframes(X_train: pd.DataFrame, X_test: pd.DataFrame = None, log_base : int = None, scale_range : tuple = None):
+def apply_smote(actigraph_data:pd.DataFrame, actigraph_labels:list, undersample_amount:float):
+    # undersample control class by a set amount
+    print(f"initial amount: {pd.Series(actigraph_labels).value_counts()}")
+    resampled_data = actigraph_data.copy()
+    if undersample_amount > 0:
+        undersample_indices = random.sample(range(actigraph_labels.count(0)), round(actigraph_labels.count(0) * undersample_amount))
+        resampled_data = resampled_data.drop(list(actigraph_data.index[undersample_indices]), axis=0)
+        resampled_labels = [actigraph_labels[i] for i in range(len(actigraph_labels)) if i not in undersample_indices]
+        print(f"undersampled amount: {pd.Series(resampled_labels).value_counts()}")
+    
+    # oversample the condition class to match the control class
+    oversample = SMOTE(sampling_strategy='minority')
+    resampled_index = list(resampled_data.index)
+    resampled_data, resampled_labels = oversample.fit_resample(resampled_data, resampled_labels)
+    
+    # create new index names for the generated samples
+    new_data_count = len(resampled_labels) - len(resampled_index)
+    new_index = [f'1_N_{i}' for i in range(new_data_count)]
+    resampled_data.index = resampled_index + new_index
+    print(f"SMOTE amount: {pd.Series(resampled_labels).value_counts()}")
+
+    return (resampled_data, list(resampled_labels))
+
+def preprocess_train_test_dataframes(
+        X_train: pd.DataFrame, X_test: pd.DataFrame = None, 
+        log_base : int = None, 
+        scale_range : tuple = None, 
+        use_standard = False, 
+        use_gaussian = None,
+    ):
     '''
     Preprocesses
     '''
+    # apply a 1d gaussian filter to each sample
+    if use_gaussian:
+        def gaussian_filter_apply(data:pd.Series):
+            return pd.Series(gaussian_filter1d(data, sigma=use_gaussian))
+        train_index = X_train.index
+        X_train = X_train.apply(gaussian_filter_apply, axis=1)
+        X_train.index = train_index
+        if X_test is not None:
+            test_index = X_test.index
+            X_test = X_test.apply(gaussian_filter_apply, axis=1)
+            X_test.index = test_index
+
     # apply log function to all values
     if log_base:
         X_train = X_train.map(lambda x: log_skip_zeroes(x, log_base))
         X_test = X_test.map(lambda x: log_skip_zeroes(x, log_base)) if X_test is not None else None
     
     # scale data to be within a specific range
-    if scale_range:
-        scaler = MinMaxScaler(scale_range)
+    if scale_range or use_standard:
+        if scale_range:
+            scaler = MinMaxScaler(scale_range)
+            if use_standard:
+                print("preprocess_train_test_dataframes(): use_standard overriden by scale_range minmax scaler")
+        elif use_standard:
+            scaler = StandardScaler()
+
         train_index = X_train.index
         X_train = pd.DataFrame(scaler.fit_transform(X_train))
         X_train.index = train_index
@@ -147,6 +197,8 @@ def preprocess_train_test_dataframes(X_train: pd.DataFrame, X_test: pd.DataFrame
             test_index = X_test.index
             X_test = pd.DataFrame(scaler.transform(X_test))
             X_test.index = test_index
+    
+    
 
     return (X_train, X_test)
 
@@ -199,27 +251,28 @@ def extract_stats_from_window(data: pd.Series, include_quarter_diff = False):
     stats.extend([h_win[1].mean() - h_win[0].mean(), h_win[1].max() - h_win[0].max(), h_win[1].min() - h_win[0].min()])
     
     # calculating quarter window statistics
-    q_win = []
-    for i in range(0, len(data_np), len(data_np)//4):
-        win = data_np[i:i+len(data_np)//4]
-        q_win.append(win)
-    
-    q_means = [win.mean() for win in q_win]
-    q_stds = [win.std() for win in q_win]
-    q_maxs = [win.max() for win in q_win]
-    q_mins = [win.min() for win in q_win]
+    if include_quarter_diff:
+        q_win = []
+        for i in range(0, len(data_np), len(data_np)//4):
+            win = data_np[i:i+len(data_np)//4]
+            q_win.append(win)
+        
+        q_means = [win.mean() for win in q_win]
+        q_stds = [win.std() for win in q_win]
+        q_maxs = [win.max() for win in q_win]
+        q_mins = [win.min() for win in q_win]
 
-    labels.extend([f'q{i}_mean' for i in range(1,len(q_win)+1)])
-    stats.extend(q_means)
-    
-    labels.extend([f'q{i}_std' for i in range(1,len(q_win)+1)])
-    stats.extend(q_stds)
-    
-    labels.extend([f'q{i}_max' for i in range(1,len(q_win)+1)])
-    stats.extend(q_maxs)
-    
-    labels.extend([f'q{i}_min' for i in range(1,len(q_win)+1)])
-    stats.extend(q_mins)
+        labels.extend([f'q{i}_mean' for i in range(1,len(q_win)+1)])
+        stats.extend(q_means)
+        
+        labels.extend([f'q{i}_std' for i in range(1,len(q_win)+1)])
+        stats.extend(q_stds)
+        
+        labels.extend([f'q{i}_max' for i in range(1,len(q_win)+1)])
+        stats.extend(q_maxs)
+        
+        labels.extend([f'q{i}_min' for i in range(1,len(q_win)+1)])
+        stats.extend(q_mins)
 
     # calculate difference in quarter statistics
     if include_quarter_diff:
@@ -285,20 +338,36 @@ def reset_feature_series(num_folds: int):
 def export_kfolds_split_indices(data: pd.DataFrame, labels: list, export_dir: str, n_splits: int, shuffle: bool, random_state: int = None):
     '''
     Splits data into N folds, writing each fold to a directory as a text file.
-
     '''
-    kf = KFold(n_splits=n_splits, 
+
+    # extracts all individual subject labels from data index
+    sample_names = list(data.index)
+    subject_names = list(dict.fromkeys(['_'.join(name.split(sep='_')[0:2]) for name in sample_names]))
+    subject_labels = [int(name[0]) for name in subject_names]
+
+    print(subject_names)
+    print(len(subject_names))
+    print(subject_labels)
+    print(len(subject_labels))
+
+    print(subject_names)
+
+    kf = StratifiedKFold(n_splits=n_splits, 
                shuffle=shuffle, 
                random_state=random_state)
-    kf.get_n_splits(data)
 
-    for i, (train_index, test_index) in enumerate(kf.split(data)):
+    for i, (train_index, test_index) in enumerate(kf.split(subject_names, subject_labels)):
+        
+        train_subjects = [name for i, name in enumerate(subject_names) if i in train_index]
+        train_names = []
+        for subject_name in train_subjects:
+            train_names += [name for name in sample_names if (subject_name + '_') in name]
+        
+        test_subjects = [name for i, name in enumerate(subject_names) if i in test_index]
+        test_names = []
+        for subject_name in test_subjects:
+            test_names += [name for name in sample_names if (subject_name + '_') in name]
 
-        X_train = data.iloc[train_index]
-        X_test = data.iloc[test_index]
-
-        train_names = list(X_train.index)
-        test_names = list(X_test.index)
         train_filename = f"fold{i}t.txt"
         test_filename = f"fold{i}e.txt"   
 

model.py
@@ -32,7 +32,8 @@ class LSTM(nn.Module):
         # print(x.shape)
         x, (hn, cn) = self.lstm(x, (h0, c0))
         # print(f"{x.shape} | {hn.shape} | {cn.shape}")
-        x = self.fc(x[:, -1, :])
+        x = x[:, -1, :]
+        x = self.fc()
         # print(x.shape)
         return x
 
@@ -47,6 +48,7 @@ class ConvNN(nn.Module):
         super().__init__()
         self.conv_block_1 = nn.Sequential(
             nn.Conv1d(in_shape, hidden_shape, kernel_size=3, padding = 1),
+            nn.BatchNorm1d(hidden_shape),
             nn.ReLU(),
             nn.Conv1d(hidden_shape, hidden_shape, kernel_size=3, padding = 1),
             nn.ReLU(),
@@ -114,26 +116,17 @@ class LSTM_Feature(nn.Module):
         
         # N * 1 * L * in_shape
         # hidden shape = number of Long term memory values
-        self.lstm = nn.LSTM(self.num_features, hidden_shape, lstm_layers, batch_first=True, dropout=0.1)
+        self.lstm = nn.LSTM(self.num_features, hidden_shape, lstm_layers, batch_first=True)
         self.fc = nn.Sequential(
-            nn.Linear(hidden_shape, hidden_shape),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
+            nn.Dropout(p=0.3),
             nn.Linear(hidden_shape, out_shape)
         )
     def forward(self, x):
-        # number of LSTM layers * L * hidden shape
-        h0 = torch.zeros(self.lstm_layers, 
-                         x.size(0), 
-                         self.hidden_shape).to(x.device)
-        c0 = torch.zeros(self.lstm_layers, 
-                         x.size(0), 
-                         self.hidden_shape).to(x.device)
         x = torch.reshape(x, (x.size(0), self.seq_len, self.num_features))
-        x, _ = self.lstm(x, (h0, c0))
-        # print(f"{x.shape} | {hn.shape} | {cn.shape}")
-        x = self.fc(x[:, -1, :])
-        # print(x.shape)
+        x, _ = self.lstm(x)
+        x = x.mean(dim=1)
+        # x = x[:, -1, :]
+        x = self.fc(x)
         return x
     
 class ConvLSTM(nn.Module):
@@ -144,41 +137,37 @@ class ConvLSTM(nn.Module):
         self.out_shape = out_shape
         self.hidden_shape = hidden_shape
         self.lstm_layers = lstm_layers
+        self.conv_hidden = 32
         
         self.conv_block = nn.Sequential(
-            nn.Conv1d(1, 32, kernel_size=5, padding=2),
+            nn.Conv1d(1, self.conv_hidden, kernel_size=3, padding=1),
+            nn.BatchNorm1d(self.conv_hidden),
             nn.ReLU(),
             nn.MaxPool1d(2),
 
-            nn.Conv1d(32, 64, kernel_size=5, padding=2),
+            nn.Conv1d(self.conv_hidden, self.conv_hidden * 2, kernel_size=3, padding=1),
+            nn.BatchNorm1d(self.conv_hidden * 2),
             nn.ReLU(),
             nn.MaxPool1d(2),
 
-            nn.Conv1d(64, 128, kernel_size=3, padding=1),
+            nn.Conv1d(self.conv_hidden * 2, self.conv_hidden * 4, kernel_size=3, padding=1),
+            nn.BatchNorm1d(self.conv_hidden * 4),
             nn.ReLU(),
-            nn.MaxPool1d(2)    
+            nn.MaxPool1d(2),
         )
-        self.lstm = nn.LSTM(hidden_shape, 
+        self.lstm = nn.LSTM(self.conv_hidden * 4, 
                             hidden_shape, 
                             lstm_layers, 
                             batch_first=True)
         self.fc = nn.Sequential(
-            nn.Linear(hidden_shape, hidden_shape),
-            nn.ReLU(),
+            nn.BatchNorm1d(hidden_shape),
             nn.Dropout(p=0.5),
             nn.Linear(hidden_shape, out_shape)
         )
     def forward(self, x):
-        h0 = torch.zeros(self.lstm_layers, 
-                         x.size(0), 
-                         self.hidden_shape).to(x.device)
-        c0 = torch.zeros(self.lstm_layers, 
-                         x.size(0), 
-                         self.hidden_shape).to(x.device)
-        
         x = self.conv_block(x)
         x = x.permute((0,2,1))
-        x, _ = self.lstm(x, (h0, c0))
-        x = self.fc(x[:, -1, :])
-
+        x, _ = self.lstm(x)
+        x = x[:,-1,:]
+        x = self.fc(x)
         return x

train_linear_svm.py
@@ -1,20 +1,21 @@
 print("--------------------------------")
-print("DEPRESSION CLASSIFICATION // V.2")
+print("DEPRESSION CLASSIFICATION // V.0")
 print("--------------------------------")
-print("PROCESS: RANDOM FOREST TRAINING")
+print("PROCESS: LINEAR SVM TRAINING")
 print("Importing libraries...")
 
 import os
-import random
 from tqdm import tqdm
+import random
 import pandas as pd
 import torch
 import torch.nn as nn
+import numpy as np
 
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import RandomizedSearchCV
+from xgboost import XGBClassifier
 from data import apply_smote, preprocess_train_test_dataframes, create_feature_dataframe, create_long_feature_dataframe
-from training_loops import run_random_forest
+from training_loops import run_linear_svc
 from eval import create_metrics_table
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -26,9 +27,8 @@ KFOLD_DIR = DATA_DIR + "/kfolds"
 KFOLD_SMOTE_DIR = DATA_DIR + "/kfolds_smote"
 NUM_FOLDS = len(os.listdir(KFOLD_DIR))//2
 RESULTS_DIR = "results"
-
 GRID_SEARCH = True
-USE_FEATURE_SERIES = True
+LONG_FEATURE = False
 
 preprocessing_grid = {
     'resample' : [False, True],
@@ -36,29 +36,12 @@ preprocessing_grid = {
     'scale_range' : [None, (0,1), (-1,1)]
 }
 
-# CHANGE TO RANDOM SEARCH CV
-hyperparameter_grid = {
-    'n_estimators': [100, 200],
-    'max_depth': [None, 10, 20],
-    'min_samples_split': [2, 5],
-    'min_samples_leaf': [1, 2],
-    'bootstrap': [True, False]
-}
-
 preprocessing_settings = {
     'resample' : True,
     'log_base' : 10,
-    'scale_range' : (-1,1),
+    'scale_range' : (0,1),
     'use_standard' : False,
-    'use_gaussian' : 100
-}
-hyperparameter_settings = {
-    'n_estimators': 200,
-    'max_depth': None,
-    'max_features': 'sqrt',
-    'min_samples_split': 2,
-    'min_samples_leaf': 2,
-    'bootstrap': True
+    'use_gaussian' : None
 }
 
 print("Loading data...")
@@ -67,12 +50,8 @@ print("Loading data...")
 data_directory = RAW_PTH
 kfolds_directory = KFOLD_DIR
 data = pd.read_csv(data_directory, index_col=0)
-
-# create feature series dataframe
-# X_feature_series_df = create_long_feature_dataframe(data.copy().drop('label'))
-# y_feature_series_df = list(data['label'])
 dataframes = []
-for i in tqdm(range(NUM_FOLDS), ncols=50):
+for i in tqdm(range(NUM_FOLDS),ncols=50):
     # extract train/test index names
     train_index = []
     test_index = []
@@ -91,7 +70,6 @@ for i in tqdm(range(NUM_FOLDS), ncols=50):
     if preprocessing_settings['resample']:
         # apply smote to training set
         X_train, y_train = apply_smote(X_train, y_train, 0.1)
-
     # preprocess data accordingly for the model
     (X_train, X_test) = preprocess_train_test_dataframes(
                             X_train=X_train,
@@ -102,12 +80,13 @@ for i in tqdm(range(NUM_FOLDS), ncols=50):
                             use_gaussian=preprocessing_settings['use_gaussian']
                         )
     # extract features
-    if USE_FEATURE_SERIES:
-        X_train = create_long_feature_dataframe(X_train)
-        X_test = create_long_feature_dataframe(X_test)
+    if LONG_FEATURE:
+        X_train = create_long_feature_dataframe(X_train, window_size=60, include_quarter_diff=False)
+        X_test = create_long_feature_dataframe(X_test, window_size=60, include_quarter_diff=False)
+        print(X_train)
     else:
-        X_train = create_feature_dataframe(X_train, include_quarter_diff=False)
-        X_test = create_feature_dataframe(X_train, include_quarter_diff=False)
+        X_train = create_feature_dataframe(X_train, True)
+        X_test = create_feature_dataframe(X_test, True)
     dataframes.append((X_train, X_test, y_train, y_test))
 
 # setup output directory, class weights, and loss function, 
@@ -119,42 +98,35 @@ class_weights_dict = {
 }
 criterion = nn.BCEWithLogitsLoss(pos_weight=class_weights).to(device)
 
-if GRID_SEARCH:
-    # CHANGE TO RANDOM SEARCH CV
-    print("Performing gridsearch...")
+# if GRID_SEARCH:
     
-    # select a random training dataframe to optimize
-    index = random.randint(0, len(dataframes) - 1)
-    (X_train, _, y_train, _) = dataframes[index]
-    # gridsearch for optimal hyperparameters
-    gridsearch = RandomizedSearchCV(estimator=RandomForestClassifier(),
-                              param_distributions=hyperparameter_grid,
-                              cv=5,
-                              verbose=True)
-    gridsearch.fit(X_train, y_train)
-    print(f"absolute cinema of params: {gridsearch.best_params_}")
-    # set settings to the optimal parameters
-    for param in gridsearch.best_params_:
-        hyperparameter_settings[param] = gridsearch.best_params_[param]
+#     print("Performing gridsearch...")
+#     # select a random training dataframe to optimize
+#     index = random.randint(0, len(dataframes) - 1)
+#     (X_train, _, y_train, _) = dataframes[index]
+#     # gridsearch for optimal hyperparameters
+#     gridsearch = RandomizedSearchCV(estimator=XGBClassifier(),
+#                               param_distributions=hyperparameter_grid,
+#                               n_iter=135,
+#                               cv=2,
+#                               verbose=True)
+#     gridsearch.fit(X_train, y_train)
+#     print(f"absolute cinema of params: {gridsearch.best_params_}")
+#     # set settings to the optimal parameters
+#     for param in gridsearch.best_params_:
+#         hyperparameter_settings[param] = gridsearch.best_params_[param]
 
 print("Evaluating model...")
 
-forest_results = run_random_forest(
+linear_svm_results = run_linear_svc(
     data=dataframes,
     criterion=criterion,
     device=device,
     weights=class_weights_dict,
-    n_estimators=hyperparameter_settings['n_estimators'],
-    max_depth=hyperparameter_settings['max_depth'],
-    max_features=hyperparameter_settings['max_features'],
-    min_samples_split=hyperparameter_settings['min_samples_split'],
-    min_samples_leaf=hyperparameter_settings['min_samples_leaf'],
-    bootstrap=hyperparameter_settings['bootstrap']
 )
-print(forest_results)
-print(hyperparameter_settings)
-forest_results.to_csv(os.path.join(RESULTS_DIR, "random_forest.csv"))
-metrics = create_metrics_table([forest_results])
+print(linear_svm_results)
+linear_svm_results.to_csv(os.path.join(RESULTS_DIR, "linear_svm.csv"))
+metrics = create_metrics_table([linear_svm_results])
 print(metrics)
 
 print("Done.")

train_lstm_v2.py
@@ -1,7 +1,7 @@
 print("--------------------------------")
 print("DEPRESSION CLASSIFICATION // V.0")
 print("--------------------------------")
-print("PROCESS: XGBOOST TRAINING")
+print("PROCESS: LSTM V2 TRAINING")
 print("Importing libraries...")
 
 import os
@@ -14,8 +14,8 @@ import numpy as np
 
 from sklearn.model_selection import RandomizedSearchCV
 from xgboost import XGBClassifier
-from data import apply_smote, preprocess_train_test_dataframes, create_feature_dataframe, create_long_feature_dataframe
-from training_loops import run_XGBoost
+from data import apply_smote, preprocess_train_test_dataframes, create_feature_dataframe, create_long_feature_dataframe, create_dataloaders
+from training_loops import run_lstm_feature
 from eval import create_metrics_table
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -27,7 +27,7 @@ KFOLD_DIR = DATA_DIR + "/kfolds"
 KFOLD_SMOTE_DIR = DATA_DIR + "/kfolds_smote"
 NUM_FOLDS = len(os.listdir(KFOLD_DIR))//2
 RESULTS_DIR = "results"
-GRID_SEARCH = False
+GRID_SEARCH = True
 LONG_FEATURE = True
 
 preprocessing_grid = {
@@ -36,24 +36,28 @@ preprocessing_grid = {
     'scale_range' : [None, (0,1), (-1,1)]
 }
 hyperparameter_grid = {
-    'min_child_weight' : [1, 5, 10],
-    'subsample' : [0.6, 0.8, 1],
-    'colsample_bytree' : [0.6, 0.8, 1],
+    'learning_rate' : [0.00025],
+    'epochs' : [200],
+    'out_shape' : [1],
+    'hidden_shape' : [16],
+    'lstm_layers' : [8]
 }
 
 preprocessing_settings = {
     'resample' : True,
-    'log_base' : 10,
+    'log_base' : None,
     'scale_range' : (0,1),
     'use_standard' : False,
-    'use_gaussian' : None
+    'use_gaussian' : None,
+    'window_size' : 30
 }
 hyperparameter_settings = {
-    'max_depth' : 6,
-    'min_child_weight' : 1,
-    'subsample' : 1,
-    'colsample_bytree' : 1,
-    'learning_rate' : 0.1
+    'learning_rate' : 0.0005,
+    'weight_decay' : 1e-4,
+    'epochs' : 50,
+    'out_shape' : 1,
+    'hidden_shape' : 64,
+    'lstm_layers' : 8
 }
 
 print("Loading data...")
@@ -93,12 +97,23 @@ for i in tqdm(range(NUM_FOLDS),ncols=50):
                         )
     # extract features
     if LONG_FEATURE:
-        X_train = create_long_feature_dataframe(X_train, window_size=60, include_quarter_diff=False)
-        X_test = create_long_feature_dataframe(X_test, window_size=60, include_quarter_diff=False)
+        X_train = create_long_feature_dataframe(X_train, 
+                                                window_size=preprocessing_settings['window_size'],
+                                                include_quarter_diff=False)
+        X_test = create_long_feature_dataframe(X_test, 
+                                               window_size=preprocessing_settings['window_size'],
+                                               include_quarter_diff=False)
+        print(X_train)
     else:
         X_train = create_feature_dataframe(X_train, True)
         X_test = create_feature_dataframe(X_test, True)
     dataframes.append((X_train, X_test, y_train, y_test))
+
+# augment data for LSTM v2 and wrap into dataloaders
+dataloaders = []
+for (X_train, X_test, y_train, y_test) in dataframes:
+    (train_dataloader, test_dataloader) = create_dataloaders(X_train, X_test, y_train, y_test, shuffle=True, batch_size=32)
+    dataloaders.append((train_dataloader, test_dataloader))
 
 # setup output directory, class weights, and loss function, 
 # aka criterion, for model training and evaluation
@@ -108,42 +123,45 @@ class_weights_dict = {
       1 : class_weights.item()
 }
 criterion = nn.BCEWithLogitsLoss(pos_weight=class_weights).to(device)
+NUM_FEATURES = len(dataframes[0][0].columns)
 
-if GRID_SEARCH:
+# if GRID_SEARCH:
     
-    print("Performing gridsearch...")
-    # select a random training dataframe to optimize
-    index = random.randint(0, len(dataframes) - 1)
-    (X_train, _, y_train, _) = dataframes[index]
-    # gridsearch for optimal hyperparameters
-    gridsearch = RandomizedSearchCV(estimator=XGBClassifier(),
-                              param_distributions=hyperparameter_grid,
-                              n_iter=135,
-                              cv=2,
-                              verbose=True)
-    gridsearch.fit(X_train, y_train)
-    print(f"absolute cinema of params: {gridsearch.best_params_}")
-    # set settings to the optimal parameters
-    for param in gridsearch.best_params_:
-        hyperparameter_settings[param] = gridsearch.best_params_[param]
+#     print("Performing gridsearch...")
+#     # select a random training dataframe to optimize
+#     index = random.randint(0, len(dataframes) - 1)
+#     (X_train, _, y_train, _) = dataframes[index]
+#     # gridsearch for optimal hyperparameters
+#     gridsearch = RandomizedSearchCV(estimator=XGBClassifier(),
+#                               param_distributions=hyperparameter_grid,
+#                               n_iter=135,
+#                               cv=2,
+#                               verbose=True)
+#     gridsearch.fit(X_train, y_train)
+#     print(f"absolute cinema of params: {gridsearch.best_params_}")
+#     # set settings to the optimal parameters
+#     for param in gridsearch.best_params_:
+#         hyperparameter_settings[param] = gridsearch.best_params_[param]
 
 print("Evaluating model...")
 
-xgb_results = run_XGBoost(
-    data=dataframes,
-    learning_rate=hyperparameter_settings['learning_rate'],
+lstm_v2_results = run_lstm_feature(
+    data=dataloaders,
     criterion=criterion,
     device=device,
-    weights=class_weights_dict,
-    max_depth=hyperparameter_settings['max_depth'],
-    min_child_weight = hyperparameter_settings['min_child_weight'],
-    subsample = hyperparameter_settings['subsample'],
-    colsample_bytree = hyperparameter_settings['colsample_bytree']
+    learning_rate=hyperparameter_settings['learning_rate'],
+    weight_decay=hyperparameter_settings['weight_decay'],
+    epochs=hyperparameter_settings['epochs'],
+    in_shape=NUM_FEATURES,
+    out_shape=hyperparameter_settings['out_shape'],
+    hidden_shape=hyperparameter_settings['hidden_shape'],
+    lstm_layers=hyperparameter_settings['lstm_layers'],
+    window_size=preprocessing_settings['window_size']
 )
-print(xgb_results)
+print(lstm_v2_results)
 print(hyperparameter_settings)
-xgb_results.to_csv(os.path.join(RESULTS_DIR, "xgb.csv"))
-metrics = create_metrics_table([xgb_results])
+lstm_v2_results.to_csv(os.path.join(RESULTS_DIR, "lstm_v2.csv"))
+metrics = create_metrics_table([lstm_v2_results])
 print(metrics)
 
 print("Done.")

train_zero_baseline.py
@@ -1,20 +1,19 @@
 print("--------------------------------")
-print("DEPRESSION CLASSIFICATION // V.2")
+print("DEPRESSION CLASSIFICATION // V.0")
 print("--------------------------------")
-print("PROCESS: RANDOM FOREST TRAINING")
+print("PROCESS: CONV NN TRAINING")
 print("Importing libraries...")
 
 import os
-import random
 from tqdm import tqdm
+import random
 import pandas as pd
 import torch
 import torch.nn as nn
+import numpy as np
 
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-from sklearn.ensemble import RandomForestClassifier
-from data import apply_smote, preprocess_train_test_dataframes, create_feature_dataframe, create_long_feature_dataframe
-from training_loops import run_random_forest
+from data import apply_smote, preprocess_train_test_dataframes, create_dataloaders
+from training_loops import run_zeroR_baseline
 from eval import create_metrics_table
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -26,9 +25,7 @@ KFOLD_DIR = DATA_DIR + "/kfolds"
 KFOLD_SMOTE_DIR = DATA_DIR + "/kfolds_smote"
 NUM_FOLDS = len(os.listdir(KFOLD_DIR))//2
 RESULTS_DIR = "results"
-
-GRID_SEARCH = True
-USE_FEATURE_SERIES = True
+LONG_FEATURE = True
 
 preprocessing_grid = {
     'resample' : [False, True],
@@ -36,29 +33,22 @@ preprocessing_grid = {
     'scale_range' : [None, (0,1), (-1,1)]
 }
 
-# CHANGE TO RANDOM SEARCH CV
-hyperparameter_grid = {
-    'n_estimators': [100, 200],
-    'max_depth': [None, 10, 20],
-    'min_samples_split': [2, 5],
-    'min_samples_leaf': [1, 2],
-    'bootstrap': [True, False]
-}
-
 preprocessing_settings = {
     'resample' : True,
-    'log_base' : 10,
-    'scale_range' : (-1,1),
+    'log_base' : None,
+    'scale_range' : (0,1),
     'use_standard' : False,
-    'use_gaussian' : 100
+    'use_gaussian' : 30,
+    'batch_size' : 32
 }
 hyperparameter_settings = {
-    'n_estimators': 200,
-    'max_depth': None,
-    'max_features': 'sqrt',
-    'min_samples_split': 2,
-    'min_samples_leaf': 2,
-    'bootstrap': True
+    'learning_rate' : 0.000005,
+    'weight_decay' : 1e-4,
+    'epochs' : 10,
+    'in_shape' : 1,
+    'out_shape' : 1,
+    'hidden_shape' : 32,
+    'flatten_factor' : 720
 }
 
 print("Loading data...")
@@ -67,12 +57,8 @@ print("Loading data...")
 data_directory = RAW_PTH
 kfolds_directory = KFOLD_DIR
 data = pd.read_csv(data_directory, index_col=0)
-
-# create feature series dataframe
-# X_feature_series_df = create_long_feature_dataframe(data.copy().drop('label'))
-# y_feature_series_df = list(data['label'])
 dataframes = []
-for i in tqdm(range(NUM_FOLDS), ncols=50):
+for i in tqdm(range(NUM_FOLDS),ncols=50):
     # extract train/test index names
     train_index = []
     test_index = []
@@ -88,10 +74,10 @@ for i in tqdm(range(NUM_FOLDS), ncols=50):
     X_test = X.drop(labels=train_index, axis=0)
     y_train = list(X_train.pop('label'))
     y_test = list(X_test.pop('label'))
+    
     if preprocessing_settings['resample']:
         # apply smote to training set
         X_train, y_train = apply_smote(X_train, y_train, 0.1)
-
     # preprocess data accordingly for the model
     (X_train, X_test) = preprocess_train_test_dataframes(
                             X_train=X_train,
@@ -101,14 +87,13 @@ for i in tqdm(range(NUM_FOLDS), ncols=50):
                             use_standard=preprocessing_settings['use_standard'],
                             use_gaussian=preprocessing_settings['use_gaussian']
                         )
-    # extract features
-    if USE_FEATURE_SERIES:
-        X_train = create_long_feature_dataframe(X_train)
-        X_test = create_long_feature_dataframe(X_test)
-    else:
-        X_train = create_feature_dataframe(X_train, include_quarter_diff=False)
-        X_test = create_feature_dataframe(X_train, include_quarter_diff=False)
     dataframes.append((X_train, X_test, y_train, y_test))
+
+# augment data for cnn and wrap into dataloaders
+dataloaders = []
+for (X_train, X_test, y_train, y_test) in dataframes:
+    (train_dataloader, test_dataloader) = create_dataloaders(X_train, X_test, y_train, y_test, shuffle=True, batch_size=32)
+    dataloaders.append((train_dataloader, test_dataloader))
 
 # setup output directory, class weights, and loss function, 
 # aka criterion, for model training and evaluation
@@ -119,42 +104,35 @@ class_weights_dict = {
 }
 criterion = nn.BCEWithLogitsLoss(pos_weight=class_weights).to(device)
 
-if GRID_SEARCH:
-    # CHANGE TO RANDOM SEARCH CV
-    print("Performing gridsearch...")
+# if GRID_SEARCH:
     
-    # select a random training dataframe to optimize
-    index = random.randint(0, len(dataframes) - 1)
-    (X_train, _, y_train, _) = dataframes[index]
-    # gridsearch for optimal hyperparameters
-    gridsearch = RandomizedSearchCV(estimator=RandomForestClassifier(),
-                              param_distributions=hyperparameter_grid,
-                              cv=5,
-                              verbose=True)
-    gridsearch.fit(X_train, y_train)
-    print(f"absolute cinema of params: {gridsearch.best_params_}")
-    # set settings to the optimal parameters
-    for param in gridsearch.best_params_:
-        hyperparameter_settings[param] = gridsearch.best_params_[param]
+#     print("Performing gridsearch...")
+#     # select a random training dataframe to optimize
+#     index = random.randint(0, len(dataframes) - 1)
+#     (X_train, _, y_train, _) = dataframes[index]
+#     # gridsearch for optimal hyperparameters
+#     gridsearch = RandomizedSearchCV(estimator=XGBClassifier(),
+#                               param_distributions=hyperparameter_grid,
+#                               n_iter=135,
+#                               cv=2,
+#                               verbose=True)
+#     gridsearch.fit(X_train, y_train)
+#     print(f"absolute cinema of params: {gridsearch.best_params_}")
+#     # set settings to the optimal parameters
+#     for param in gridsearch.best_params_:
+#         hyperparameter_settings[param] = gridsearch.best_params_[param]
 
 print("Evaluating model...")
 
-forest_results = run_random_forest(
-    data=dataframes,
+zeroR_results = run_zeroR_baseline(
+    data=dataloaders,
     criterion=criterion,
     device=device,
-    weights=class_weights_dict,
-    n_estimators=hyperparameter_settings['n_estimators'],
-    max_depth=hyperparameter_settings['max_depth'],
-    max_features=hyperparameter_settings['max_features'],
-    min_samples_split=hyperparameter_settings['min_samples_split'],
-    min_samples_leaf=hyperparameter_settings['min_samples_leaf'],
-    bootstrap=hyperparameter_settings['bootstrap']
 )
-print(forest_results)
+print(zeroR_results)
 print(hyperparameter_settings)
-forest_results.to_csv(os.path.join(RESULTS_DIR, "random_forest.csv"))
-metrics = create_metrics_table([forest_results])
+zeroR_results.to_csv(os.path.join(RESULTS_DIR, "zeroR.csv"))
+metrics = create_metrics_table([zeroR_results])
 print(metrics)
 
 print("Done.")

training_loops.py
@@ -10,17 +10,20 @@ from xgboost import XGBClassifier
 
 from engine import train_test
 from eval import eval_model, eval_sklearn_model, append_weighted_average
-from model import ZeroR, ConvNN, LSTM, FeatureMLP, LSTM_Feature
+from model import ZeroR, ConvNN, LSTM, FeatureMLP, LSTM_Feature, ConvLSTM
 
 def run_linear_svc(data: list, criterion, device, weights = None):
     print("Linear SVM Classifier:")
     
     linear_svc_results = []
     for i, (X_train, X_test, y_train, y_test) in enumerate(tqdm(data, ncols=50)):
+        print(f"{i}, running!")
         # reset model
         model = SVC(kernel='linear', probability=True, class_weight=weights)
+        print(X_train)
         # train model
         model.fit(X_train, y_train)
+        print("done training")
         linear_svc_results.append(
                 eval_sklearn_model(model = model,
                                 note = f"{i}",
@@ -166,7 +169,7 @@ def run_cnn(data: list, criterion, device, learning_rate, epochs, in_shape, out_
             optimizer = torch.optim.Adam(params = model_0.parameters(), lr=learning_rate)
             # train model
             train_test(model_0, train_dataloader, test_dataloader, epochs = epochs, optimizer=optimizer, 
-                  criterion=criterion, device=device, verbose=False)
+                  criterion=criterion, device=device, verbose=True)
             cnn_results.append(
                   eval_model(model = model_0,
                         note = f"{i}",
@@ -203,7 +206,7 @@ def run_mlp(data: list, criterion, device, learning_rate, epochs, in_shape, out_
       mlp_results = append_weighted_average(mlp_results)
       return mlp_results
 
-def run_lstm_feature(data: list, criterion, device, learning_rate, epochs, in_shape, out_shape, hidden_shape, lstm_layers, window_size):
+def run_lstm_feature(data: list, criterion, device, learning_rate, weight_decay, epochs, in_shape, out_shape, hidden_shape, lstm_layers, window_size):
       print("LSTM V2:")
       
       lstm_series_results = []
@@ -215,7 +218,7 @@ def run_lstm_feature(data: list, criterion, device, learning_rate, epochs, in_sh
       for i, (train_dataloader, test_dataloader) in enumerate(tqdm(data, ncols=50)):
             # reset model
             model_3 = LSTM_Feature(IN_3, OUT_3, HIDDEN_3, LSTM_LAYERS, WINDOW_SIZE).to(device)
-            optimizer = torch.optim.Adam(params = model_3.parameters(), lr=learning_rate)
+            optimizer = torch.optim.AdamW(params = model_3.parameters(), lr=learning_rate, weight_decay=weight_decay)
             # train model
             train_test(model_3, train_dataloader, test_dataloader, epochs = epochs, optimizer=optimizer, 
                   criterion=criterion, device=device, verbose=True)
@@ -229,3 +232,29 @@ def run_lstm_feature(data: list, criterion, device, learning_rate, epochs, in_sh
       lstm_series_results = pd.concat(lstm_series_results, axis=1).transpose()
       lstm_series_results = append_weighted_average(lstm_series_results)
       return lstm_series_results
+
+def run_conv_lstm(data: list, criterion, device, learning_rate, weight_decay, epochs, in_shape, out_shape, hidden_shape, lstm_layers):
+      print("CONV LSTM:")
+      
+      conv_lstm_results = []
+      IN_3 = in_shape
+      OUT_3 = out_shape
+      HIDDEN_3 = hidden_shape
+      LSTM_LAYERS = lstm_layers
+      for i, (train_dataloader, test_dataloader) in enumerate(tqdm(data, ncols=50)):
+            # reset model
+            model_3 = ConvLSTM(IN_3, OUT_3, HIDDEN_3, LSTM_LAYERS).to(device)
+            optimizer = torch.optim.AdamW(params = model_3.parameters(), lr=learning_rate, weight_decay=weight_decay)
+            # train model
+            train_test(model_3, train_dataloader, test_dataloader, epochs = epochs, optimizer=optimizer, 
+                  criterion=criterion, device=device, verbose=True)
+            conv_lstm_results.append(
+                  eval_model(model = model_3,
+                        note = f"{i}",
+                        dataloader = test_dataloader,
+                        criterion = criterion,
+                        device = device)
+            )
+      conv_lstm_results = pd.concat(conv_lstm_results, axis=1).transpose()
+      conv_lstm_results = append_weighted_average(conv_lstm_results)
+      return conv_lstm_results